0efbe69bc6bf9198174cd19605b89801202a3eb6
Niklas Carlsson <niklas.carlsson1994@gmail.com>
2017-09-08 20:05:25 +0200

    hej

--- lab2/dataflow.scala
+++ lab2/dataflow.scala
@@ -8,6 +8,12 @@ case class Stop();
 case class Ready();
 case class Go();
 case class Change(in: BitSet);
+case class Run()
+case class StopAll()
+//case class Quit()
+case class RequestIn()
+case class Compute()
+case class GoAgain()
 
 class Random(seed: Int) {
         var w = seed + 1;
@@ -28,6 +34,8 @@ class Controller(val cfg: Array[Vertex]) extends Actor {
 
   // LAB 2: The controller must figure out when
   //        to terminate all actors somehow.
+  
+  var computing = 0
 
   def act() {
     react {
@@ -40,6 +48,35 @@ class Controller(val cfg: Array[Vertex]) extends Actor {
         }
         act();
       }
+      
+      case Run() => {
+          computing += 1
+          
+          //printf("run: computed %d\n", computing)
+          
+          act()
+      }
+      
+      case Stop() => {
+          computing -= 1
+          
+          //printf("stop: computed %d\n", computing)
+          
+          if(computing <= 0) {
+              cfg.foreach(v => v ! Stop())
+              //cfg.foreach(v => v.print)
+          }
+          
+          else {
+              act()
+          }
+      }
+      
+      /*
+      case StopAll() => {
+          cfg.foreach(v => v ! Quit())
+      }
+      */
     }
   }
 }
@@ -51,7 +88,10 @@ class Vertex(val index: Int, s: Int, val controller: Controller) extends Actor {
   val defs               = new BitSet(s);
   var in                 = new BitSet(s);
   var out                = new BitSet(s);
-
+  //var working            = false
+  //var received           = 0
+  //var listed             = false
+  
   def connect(that: Vertex)
   {
     //println(this.index + "->" + that.index);
@@ -69,16 +109,74 @@ class Vertex(val index: Int, s: Int, val controller: Controller) extends Actor {
 
       case Go() => {
         // LAB 2: Start working with this vertex.
-        var old = in;
-        var i = 0;
         
-        for(v <- succ) {
-        }
+        controller ! new Run
+        //received = 0
+        succ.foreach(v => v ! new RequestIn)
         
         act();
       }
+      
+      case GoAgain() => {
+          this ! new Go()
+          
+          act()
+      }
+      /*
+      case GoAgain() => {
+          if(!listed) {
+              this ! new Go
+          }
+          
+          else {
+              controller ! new Stop
+          }
+          
+          act()
+      }
+      */
 
       case Stop()  => { }
+      
+      case RequestIn() => {
+          sender ! new Change(in)
+          
+          act()
+      }
+      
+      case Change(in: BitSet) => {
+          out.or(in)
+          //received += 1
+          
+          //printf("%d: received %d\n", index, received)
+          
+          /*
+          if(received == succ.length) {
+              this ! new Compute
+          }
+          */
+          
+          this ! new Compute
+          
+          act()
+      }
+      
+      case Compute() => {
+          var old = in
+          
+          in = new BitSet()
+          in.or(out)
+          in.andNot(defs)
+          in.or(uses)
+          
+          if(!in.equals(old)) {
+              pred.foreach(v => v ! new GoAgain)
+          }
+          
+          controller ! new Stop
+          
+          act()
+      }
     }
   }
 
